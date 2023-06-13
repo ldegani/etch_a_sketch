18f097ff1ad72baa58e14ce082804352a39621fd
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-06-13 16:15:21 -0300

    Add ERASER button and fix footer

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.grid-container');
+const eraserButton = document.querySelector('.eraser');
 
 const contents = [];
+let eraser = false;
 
 for (let i = 0; i <= 255; i++) {
   contents[i] = `content${[i]}`;
@@ -10,16 +12,28 @@ for (let i = 0; i <= contents.length - 1; i++) {
   console.log(contents[i]);
   contents[i] = document.createElement('div');
   contents[i].classList.add('grid');
-//  contents[i].setAttribute('id', 'c'+[i]);
   container.appendChild(contents[i]);
 }
 
 const grids = document.querySelectorAll('.grid');
 
 for (let i = 0; i <= grids.length - 1; i++) {
-  grids[i].addEventListener('click', (event) => {
-    const item = event.target;
-    item.classList.remove('grid');
-    item.classList.add('grid-selected');
+  grids[i].addEventListener('click', () => {
+    if (eraser) {
+      grids[i].classList.remove('selected');
+    } else {
+      grids[i].classList.add('selected');
+    }
   })
 };
+
+eraserButton.addEventListener('click', () => {
+  eraserButton.classList.toggle('eraser-active');
+  if (eraser) {
+    eraser = false;
+    console.log(eraser);
+  } else {
+    eraser = true;
+    console.log(eraser);
+  }
+})
