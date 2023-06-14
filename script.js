b1e9884d60a3feda4588d8500a0638cde27d76db
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-06-14 14:05:55 -0300

    Implement size changing and cleaner code

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 const container = document.querySelector('.grid-container');
 const eraserButton = document.querySelector('.eraser');
+const sizeButton = document.querySelector('.size');
+const rainbowButton = document.querySelector('.rainbow');
 
 const contents = [];
 let eraser = false;
 let grids; // Declared as a global variable to be reachable
+let size = 16; // Declared as a global variable to be reachable
 
-function gridCreation() {
-  for (let i = 0; i <= 255; i++) {
+eraserButton.onclick = erase; // Calls the function only when clicking
+sizeButton.onclick = takeSize();
+rainbowButton.onclick = rainbowMode;
+
+function gridCreation(size) {
+  cellSize = 100 / size;
+  for (let i = 1; i <= size * size; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
+    grid.style.flexBasis = `${cellSize}%`;
     container.appendChild(grid);
   }
 
   grids = document.querySelectorAll('.grid'); // Assign 'grids' globally
   paintSquare(); // Call the function to attach the event listener
-  erase(); // Call the function to initialize it
+}
+
+function takeSize() {
+  sizeButton.addEventListener('click', () => {
+    size = parseInt(prompt('Tell me how many squares you need:'));
+    if (isNaN(size) || size <=0 || size > 100) {
+      alert('Invalid entry! Try a number between 1 and 100');
+      return;
+    }
+    clearGrid();
+    gridCreation(size);
+  });
 }
 
 function paintSquare() {
@@ -29,15 +49,20 @@ function paintSquare() {
   };
 }
 
-function erase(){
-  eraserButton.addEventListener('click', () => {
-    eraserButton.classList.toggle('eraser-active');
-    if (eraser) {
-      eraser = false;
-    } else {
-      eraser = true;
-    }
-  })
+function rainbowMode() {
+  rainbowButton.classList.toggle('active');
 }
 
-gridCreation()
+function clearGrid() {
+  //container.innerHTML = '';
+  while(container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+function erase(){
+  eraserButton.classList.toggle('active');
+  eraser = !eraser;
+}
+
+gridCreation(size)
