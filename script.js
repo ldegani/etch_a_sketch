1314538cdac08a3dea10a1d511968d3b3cc55e9c
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2023-06-14 16:49:29 -0300

    Implement CLEAR button and change layout

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ const container = document.querySelector('.grid-container');
 const eraserButton = document.querySelector('.eraser');
 const sizeButton = document.querySelector('.size');
 const rainbowButton = document.querySelector('.rainbow');
+const clearButton = document.querySelector('.clear');
 
 const contents = [];
 let eraser = false;
+let rainbow = false;
 let grids; // Declared as a global variable to be reachable
-let size = 16; // Declared as a global variable to be reachable
+let defaultGrid = 16;
 
 eraserButton.onclick = erase; // Calls the function only when clicking
-sizeButton.onclick = takeSize();
+sizeButton.onclick = takeSize;
 rainbowButton.onclick = rainbowMode;
+clearButton.onclick = clearDrawing;
 
-function gridCreation(size) {
-  cellSize = 100 / size;
-  for (let i = 1; i <= size * size; i++) {
+function gridCreation(newSize) {
+  cellSize = 100 / newSize;
+  for (let i = 1; i <= newSize * newSize; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
     grid.style.flexBasis = `${cellSize}%`;
@@ -26,22 +29,24 @@ function gridCreation(size) {
 }
 
 function takeSize() {
-  sizeButton.addEventListener('click', () => {
-    size = parseInt(prompt('Tell me how many squares you need:'));
-    if (isNaN(size) || size <=0 || size > 100) {
-      alert('Invalid entry! Try a number between 1 and 100');
-      return;
-    }
-    clearGrid();
-    gridCreation(size);
-  });
+  newSize = parseInt(prompt('Tell me how many squares you need:'));
+  if (isNaN(newSize) || newSize <=0 || newSize > 100) {
+    alert('Invalid entry! Try a number between 1 and 100');
+    return;
+  }
+  clearGrid();
+  gridCreation(newSize);
 }
 
 function paintSquare() {
   for (let i = 0; i <= grids.length - 1; i++) {
     grids[i].addEventListener('click', () => {
+      if (rainbow) {
+        grids[i].style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+      }
       if (eraser) {
         grids[i].classList.remove('selected');
+        grids[i].style.backgroundColor = '';
       } else {
        grids[i].classList.add('selected');
       }
@@ -51,6 +56,11 @@ function paintSquare() {
 
 function rainbowMode() {
   rainbowButton.classList.toggle('active');
+  rainbow = !rainbow;
+}
+
+function randomColor() {
+  return Math.floor(Math.random() * 256);
 }
 
 function clearGrid() {
@@ -60,9 +70,16 @@ function clearGrid() {
   }
 }
 
+function clearDrawing() {
+  for (let i = 0; i <= grids.length; i++) {
+    grids[i].classList.remove('selected');
+    grids[i].style.backgroundColor = '';
+  }
+}
+
 function erase(){
   eraserButton.classList.toggle('active');
   eraser = !eraser;
 }
 
-gridCreation(size)
+gridCreation(defaultGrid)
